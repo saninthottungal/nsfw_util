a6ba3da04d5b4fc26229d6515a9f378abfd84c25
sanin <saninthottungalhere@gmail.com>
2025-12-15 18:35:44 +0530

    new model added

--- lib/src/utils/asset_utils.dart
+++ lib/src/utils/asset_utils.dart
@@ -1,6 +1,6 @@
 class Assets {
   const Assets._();
 
-  static const String model = 'packages/nsfw_util/assets/models/nsfw.tflite';
+  static const String model = 'packages/nsfw_util/assets/models/nsfw_dr.tflite';
   static const String labels = 'packages/nsfw_util/assets/models/labels.txt';
 }
